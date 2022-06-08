ea2aeec1d6f2e9df0ef22c3ebca1914c1509255f
Phatsakorn Rodphol (NTL) <CF303957@tidlor.com>
2022-06-08 17:01:58 +0700

    init my app

---
+++ HW2/Job.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW2
+{
+    public class Doctor
+    {
+        private string Name { get; set; }
+        private string Occupation { get; set; }
+        private decimal Salary { get; set; }
+        private decimal Tax { get; set; }
+
+
+        public Doctor(string name, string occupation, decimal salary, decimal tax)
+        {
+            this.Name = name;
+            this.Occupation = occupation;
+            this.Salary = salary;
+            this.Tax = tax;
+        }
+        public string WhoIAM()
+        {
+            
+            var response = Name+" is a "+Occupation;
+            return response;
+            
+          
+        }
+        public decimal GetTax()
+        {
+            return ((Salary * Tax) / 100);
+      
+
+        }
+    }
+}
---
+++ HW2/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+using HW2;
+Console.WriteLine("========Doctor=========");
+Console.WriteLine("Enter your name : ");
+var myName = Console.ReadLine();
+
+Console.WriteLine("Enter your occupation : ");
+var myOccupation = Console.ReadLine();
+
+Console.WriteLine("Enter your salary : ");
+var mySalary = Console.ReadLine();
+
+Console.WriteLine("Enter your tax : ");
+var myTax = Console.ReadLine();
+
+var Salary = decimal.Parse(mySalary);
+var Tax = decimal.Parse(myTax);
+
+var job = new Doctor(myName, myOccupation, Salary, Tax);
+var Jobs = job.WhoIAM();
+
+
+Console.WriteLine(Jobs);
+
+var Doctor = job.GetTax();
+Console.WriteLine(Doctor);
+
+Console.WriteLine("========Police=========");
+
+Console.WriteLine("========Teacher=========");
